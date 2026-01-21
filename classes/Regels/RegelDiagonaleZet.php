1242a7ec00e6c494098cbb92c1a7fded233f0692
markpopa <popamarknicolas1@gmail.com>
2026-01-21 15:30:50 +0100

    ANotha one

---
+++ classes/Regels/RegelDiagonaleZet.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+class RegelDiagonaleZet implements RegelInterface
+{
+    public function check(Zet $zet, Bord $bord, string $huidigeSpeler): bool
+    {
+        $start = $zet->getVanPositie();
+        $eind  = $zet->getNaarPositie();
+
+        $dx = abs($eind->getX() - $start->getX());
+        $dy = abs($eind->getY() - $start->getY());
+
+        if ($dx !== $dy || $dx > 2) {
+            echo "Fout: Een zet moet diagonaal en maximaal 2 stappen zijn." . PHP_EOL;
+            return false;
+        }
+
+        return true;
+    }
+}
